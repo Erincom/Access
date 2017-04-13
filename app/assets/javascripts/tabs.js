d15053385ed66b62bf0a3e2b37b7ec8d26b1c621
Erin Douglas <erin.douglas@riseup.net>
2017-04-13 14:04:34 +0200

    Functioning student page tabs

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
-  $('#nav-tabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show');
-    $(this).parent().addClass("active");
-    $(this).parent().siblings().removeClass("active");
-    var tab = $(this).attr("href");
-    $(".tab").not(tab).hide();
-    $(tab).fadeIn();
-  });
-});
+// $(document).ready(function() {
+// //  $('#classes').hide();
+// //  $('#michigan-tests').hide();
+//   $('.nav-tabs a').click(function (e) {
+// //    e.preventDefault()
+// //    $(this).tab.('show');
+//     $(this).parent().addClass("active");
+//     $(this).parent().siblings().removeClass("active");
+
+//     var tab = $(this).attr("href");
+// //    $('.tab-content').hide();
+//     $(tab).tab('show');
+
+// //    $(".tab-content").not(tab).hide();
+// });
+// });
