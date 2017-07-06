3c578c25f3dd83459d3469c9eb5d7844447d25d6
Erin Douglas <erin.douglas@riseup.net>
2017-07-06 17:39:25 +0200

    The attendance report view generates the correct list of students and their attendance

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,9 +4,10 @@ class ReportsController < ApplicationController
     if params[:company].present? && params[:session].present?
       @company = params[:company]
       @session = Session.find(params[:session])
+
       company_students = Student.by_company(@company)
-      lesson_ids = @session.lessons.pluck(:classid)
-      student_array = StudentLesson.where(lesson: lesson_ids, student: company_students).group_by(&:student).keys
+      @lesson_ids = @session.lessons.pluck(:classid)
+      student_array = StudentLesson.where(lesson: @lesson_ids, student: company_students).group_by(&:student).keys
       students = student_array.select(&:studentid)
       @students = students.sort_by(&:lastname)
       # @lessons_per_student = Lesson.where(session: @session).group(:student_id).group_by(&:student)
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,23 +1,25 @@
 module ReportsHelper
   include MetaclassesHelper
 
-  def calculate_hours_attended(student, metaclass_array)
-    attended_classes = 0
-    metaclass_array.each do |metaclass|
-      metaclass.lessons.each do |lesson|
-        attended_classes += 1 if lesson.student_lessons.where(student: student).first.attendance == true
-      end
+  def calculate_hours_attended(student, student_lesson_array)
+    attended_hours = 0
+    lesson_ids = student_lesson_array.pluck(:classid)
+    lesson_ids.each do |lesson_id|
+      lesson = Lesson.find(lesson_id)
+      duration = calculate_hours(lesson.metaclass.metaclassdaytime)
+        attended_hours += (1 * duration) if lesson.student_lessons.where(student: student).first.attendance == true
     end
-    attended_classes
+    attended_hours
   end
 
-  def calculate_total_hours(metaclass_array)
+  def calculate_total_hours(student, student_lesson_array)
     total_hours = 0
-    metaclass_array.each do |metaclass|
-      number_classes = metaclass.lessons.size
-      number_hours = calculate_hours(metaclass.metaclassdaytime)
-      total_metaclass_hours = number_classes * number_hours
-      total_hours += total_metaclass_hours
+    lesson_ids = student_lesson_array.pluck(:classid)
+    lesson_ids.each do |lesson_id|
+      lesson = Lesson.find(lesson_id)
+      duration = calculate_hours(lesson.metaclass.metaclassdaytime)
+      hours = 1 * duration
+      total_hours += hours
     end
     total_hours
   end
